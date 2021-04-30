960dcb8ef5d768f855ad8f929ae0b1d1b5da8a03
Ricky Liu <55927555+RiccLQL@users.noreply.github.com>
2021-04-30 16:53:13 -0400

    Update server.js

--- backend/app/src/server.js
+++ backend/app/src/server.js
@@ -32,6 +32,7 @@ const app = express();
 app.use(
     cors({
         origin: config.backend.BACKEND_DOMAIN,
+        "Access-Control-Allow-Origin": config.backend.BACKEND_DOMAIN,
         methods: "GET,PUT,POST,DELETE",
     })
 );
