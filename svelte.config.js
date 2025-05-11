88334c104fcca0d4f0499dfaefeee51d0e7f5251
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-05-11 16:20:49 -0400

    Add `adapter-node` for production on EUS server deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
