d2b995ac6d9879753960acc8e431669bcbd34daf
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-06-05 18:07:39 +0100

    Office Hours query

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,19 @@
 import { getFromCMS } from 'utils/utils.js';
 
 // needs to concat and format this text
-const query = `*[_type == "homepage"].description[].children[].text`;
+const descQuery = `*[_type == "homepage"].description[].children[].text`;
+
+const ohQuery = `*[_type=="oh"].schedule[]{
+  day,
+  startTime,
+  endTime,
+  "host": member->name
+}`;
 
 export const load = async () => {
 	return {
-		description: await getFromCMS(query),
-		ohs: "",
+		description: await getFromCMS(descQuery),
+		ohs: await getFromCMS(ohQuery),
 		pictures: "",
 		FAQs: "",
 	};
